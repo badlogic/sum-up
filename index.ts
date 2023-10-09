0c222f0020995ac1d5c17fbc191c7705a63f18a2
Mario Zechner <badlogicgames@gmail.com>
2023-10-10 01:09:37 +0200

    And done. I think.

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
-import { LitElement, html, nothing } from "lit";
-import { customElement, state } from "lit/decorators.js";
+import { LitElement, PropertyValueMap, html, nothing } from "lit";
+import { unsafeHTML } from "lit/directives/unsafe-html.js";
+import { customElement, query, state } from "lit/decorators.js";
 import { globalStyles } from "./styles";
+
+export type AccountSummary = { text: string; displayName: string; avatar: string };
 
 export function get(url: string) {
     if (location.href.includes("localhost")) {
@@ -16,19 +19,98 @@ export class App extends LitElement {
     @state()
     message?: string;
 
+    @state()
+    avatar?: string;
+
+    @state()
+    displayName?: string;
+
+    @query("#input")
+    input?: HTMLInputElement;
+
+    @query("#load")
+    load?: HTMLInputElement;
+
+    loading = false;
+
+    account: string | null;
+
     constructor() {
         super();
-        this.load();
+        this.account = new URL(window.location.href).searchParams.get("account");
     }
 
-    async load() {
-        const response = await get("/api/test");
-        if (response.status != 200) this.message = "Error";
-        else this.message = (await response.json()).result;
+    protected firstUpdated(_changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>): void {
+        if (this.account) this.hitMe();
     }
 
     render() {
-        return html`<h1>This is a test</h1>
-            ${this.message ? html`<div>${this.message}</div>` : nothing}`;
+        return html` <div class="flex flex-col items-center max-w-[600px] m-auto">
+            <a class="text-center text-primary font-bold text-2xl my-4" href="/">Sum-up</a>
+            <div class="text-center mb-4">See what ChatGPT thinks of your Bluesky feed</div>
+            <div class="flex">
+                <input
+                    id="input"
+                    class="flex-1 border rounded-l px-2 py-1"
+                    placeholder="Bluesky handle, e.g. badlogic.bsky.social"
+                    value="${this.account || nothing}"
+                />
+                <button id="load" class="bg-primary text-white rounded-r px-4" @click=${() => this.hitMe()}>Hit me</button>
+            </div>
+            ${this.loading
+                ? html`<div class="mt-4 text-center m-auto">
+                          Asking ChatGPT to tell you how swell you are. <br />
+                          This could take a while. Look at the cat.
+                      </div>
+                      <img class="w-[100px]" src="cat-nyan-cat.gif" />`
+                : nothing}
+            ${this.avatar ? html`<img class="my-4 rounded-full max-w-[150px] max-h-[150px]" src="${this.avatar}" />` : nothing}
+            ${this.displayName
+                ? html`<div class="text-bold text-2xl">${this.displayName}</div>
+                      <a class="mb-4 font-bold text-primary text-center" href="${location.href}">Share</a>`
+                : nothing}
+            ${this.message ? html`<div>${unsafeHTML(this.message)}</div>` : nothing}
+            <div class="flex-1"></div>
+            <div class="text-center italic mt-4">
+                Lovingly made by <a class="text-primary" href="https://bsky.app/profile/badlogic.bsky.social">Mario Zechner</a><br />
+                No data is collected, not even your IP address.
+                <a class="text-primary" href="https://github.com/badlogic/sum-up"><br />Source code</a>
+            </div>
+        </div>`;
+    }
+
+    async hitMe() {
+        this.input!.disabled = true;
+        this.load!.disabled = true;
+
+        const account = this.input!.value.trim();
+        if (account.length == 0) {
+            this.message = "Please specify an account";
+            this.input!.disabled = false;
+            this.load!.disabled = false;
+        }
+
+        this.avatar = undefined;
+        this.displayName = undefined;
+        this.message = undefined;
+        this.loading = true;
+        this.requestUpdate();
+
+        const response = await get(`/api/summarize/${account}`);
+        if (response.status != 200) {
+            this.avatar = undefined;
+            this.message = "Whoopsies, something went wrong.";
+        } else {
+            const reply = (await response.json()) as { accountSummary: { text: string; displayName: string; avatar: string }; gptSummary: string };
+            this.avatar = reply.accountSummary.avatar;
+            this.displayName = reply.accountSummary.displayName;
+            this.message = reply.gptSummary.replace("Summary:", "").trim().replaceAll("\n", "<br>");
+            const url = new URL(window.location.href);
+            url.searchParams.set("account", account);
+            history.replaceState(null, "", url.toString());
+        }
+        this.input!.disabled = false;
+        this.load!.disabled = false;
+        this.loading = false;
     }
 }
--- server.ts
+++ server.ts
@@ -1,25 +1,160 @@
 import compression from "compression";
-import express from "express";
+import express, { json } from "express";
 import * as http from "http";
 import cors from "cors"; // Import the cors middleware
+import OpenAI from "openai";
 
-const port = process.env.PORT;
+const cache = new Map<String, { accountSummary: AccountSummary; gptSummary: string }>();
+
+const port = process.env.PORT ?? 3333;
 const openAiKey = process.env.SUMUP_OPENAI;
 const blueskyAccount = process.env.SUMUP_BLUESKY_ACCOUNT;
-const blueskyKey = process.env.SUMUP_BLUESKY_KEY;
+const blueskyPassword = process.env.SUMUP_BLUESKY_PASSWORD;
 
-console.log(`OpenAI key: ${openAiKey}`);
-console.log(`BlueSky account: ${blueskyAccount}`);
-console.log(`BlueSky key: ${blueskyKey}`);
+if (!openAiKey) {
+    console.error("No OpenAI key given");
+    process.exit(-1);
+}
 
-const app = express();
-app.use(cors());
-app.use(compression());
-app.use(express.static("site"));
-app.get("/api/test", async (req, res) => {
-    res.json({ result: "Test" });
+if (!blueskyAccount) {
+    console.error("No Bluesky account given");
+    process.exit(-1);
+}
+
+if (!blueskyPassword) {
+    console.error("No Bluesky account given");
+    process.exit(-1);
+}
+
+const openai = new OpenAI({
+    apiKey: openAiKey,
 });
 
-http.createServer(app).listen(port, () => {
-    console.log(`App listening on port ${port}`);
-});
+(async () => {
+    console.log(`OpenAI key: ${openAiKey}`);
+    console.log(`BlueSky account: ${blueskyAccount}`);
+    console.log(`BlueSky password: ${blueskyPassword}`);
+
+    const session = await createSession(blueskyAccount, blueskyPassword);
+    if (!session) process.exit(-1);
+
+    const app = express();
+    app.use(cors());
+    app.use(compression());
+    app.use(json());
+    app.use(express.static("site"));
+    app.get("/api/summarize/:accountName", async (req, res) => {
+        const account = req.params.accountName;
+        console.log(`Summarizing account ${account}`);
+        if (cache.has(account)) {
+            res.json(cache.get(account));
+            return;
+        }
+
+        try {
+            const accountSummary = await getUserPosts(account, session);
+            if (!accountSummary) {
+                res.status(400);
+                return;
+            }
+            const gptSummary = await getSummary(accountSummary.text, accountSummary.displayName);
+            const result = { accountSummary, gptSummary };
+            cache.set(account, result);
+            res.json({ accountSummary, gptSummary });
+        } catch (e) {
+            console.log(`Error ${JSON.stringify(e, null, 2)}`);
+            res.status(400);
+        }
+    });
+
+    http.createServer(app).listen(port, () => {
+        console.log(`App listening on port ${port}`);
+    });
+})();
+
+type BskySession = { did: string; handle: string; email: string; accessJwt: string; refreshJwt: string };
+
+async function createSession(accountName: string, password: string): Promise<BskySession | null> {
+    const url = "https://bsky.social/xrpc/com.atproto.server.createSession";
+
+    const data = {
+        identifier: accountName,
+        password: password,
+    };
+
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+
+        if (response.ok) {
+            const responseData = await response.json();
+            console.log("Session created:", responseData);
+            return responseData as BskySession;
+        } else {
+            console.error("Failed to create session:", response.status, response.statusText);
+            return null;
+        }
+    } catch (error) {
+        console.error("Error creating session" + JSON.stringify(error, null, 2));
+        return null;
+    }
+}
+
+async function getSummary(userText: string, displayName: string): Promise<string> {
+    const prompt = `
+    summarize this bluesky user's posts in a humorous way. the summary should be at least 6 paragraphs long. use sarcasm to spice things up. it is absolutely crucial that you use the language that most of the user's posts are written in for the summary! seriously, use the language found in the posts! this is very important! their display name is ${displayName}.`;
+    try {
+        const chatCompletion = await openai.chat.completions.create({
+            messages: [{ role: "user", content: prompt + "\n\n" + userText }],
+            model: "gpt-3.5-turbo",
+            temperature: 0.7,
+        });
+        return chatCompletion.choices[0].message.content!;
+    } catch (error) {
+        console.error("Error generating response:", error);
+        throw error;
+    }
+}
+
+export type BskyFeed = {
+    cursor: string;
+    feed: BskyPost[];
+};
+
+export type BskyPost = {
+    post: {
+        author: {
+            avatar: string;
+            did: string;
+            displayName: string;
+            handle: string;
+        };
+        record: {
+            text: string;
+        };
+    };
+};
+
+export type AccountSummary = { text: string; displayName: string; avatar: string };
+
+async function getUserPosts(account: string, session: BskySession): Promise<AccountSummary | null> {
+    const url = new URL("https://bsky.social/xrpc/app.bsky.feed.getAuthorFeed");
+    url.searchParams.append("actor", account.replaceAll("@", ""));
+    url.searchParams.append("limit", (100).toString());
+
+    const response = await fetch(url.toString(), {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${session.accessJwt}`,
+        },
+    });
+    if (response.status != 200) return null;
+    const feed = (await response.json()) as BskyFeed;
+    const text = feed.feed.map((post) => post.post.record.text).join(" ");
+    return { text, displayName: feed.feed[0].post.author.displayName, avatar: feed.feed[0].post.author.avatar };
+}
