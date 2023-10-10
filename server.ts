f1ddc741eec392f9ed1471fbc188d78715c6adf3
Mario Zechner <badlogicgames@gmail.com>
2023-10-10 02:11:19 +0200

    Maybe someday a better prompt will be found. Today is not the day.

--- server.ts
+++ server.ts
@@ -107,7 +107,7 @@ async function createSession(accountName: string, password: string): Promise<Bsk
 
 async function getSummary(userText: string, displayName: string): Promise<string> {
     const prompt = `
-    summarize this bluesky user's posts in a humorous way. the summary should be at least 6 paragraphs long. use sarcasm to spice things up. it's ok to be sarcastically negative, but do not be mean. their display name is ${displayName}.`;
+    summarize this bluesky user's posts in a humorous way. the summary should be at least 6 paragraphs long. don't make it a direct summary, but make the summary kind of about the person themselves. use sarcasm to spice things up. it's ok to be sarcastically negative, but do not be mean. their display name is ${displayName}.`;
     try {
         const chatCompletion = await openai.chat.completions.create({
             messages: [{ role: "user", content: prompt + "\n\n" + userText }],
